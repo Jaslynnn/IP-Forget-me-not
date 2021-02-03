9a4df88af4fcdfa0b8648adb3784507923e7a2b9
Jaslynnn <s10204654@connect.np.edu.sg>
2021-02-03 21:27:11 +0800

    alerts and coin system

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// record the user's house door location 
+// record the user's house door location
+
+let points = localStorage.getItem("Points")  
+document.getElementById("points").innerHTML = points
+
+let checklist = localStorage.getItem("Checklist")  
+for (var i = 0; i < checklist.length; i++) {
+  document.getElementById("list").innerHTML +=
+    ` ${checklist[i]} ${i}  `
+}
+
 function recordPosition() {
   const successCallback = (position) => {
     console.log(position);
@@ -59,6 +69,7 @@ function init() {
 $(document).ready(function () {
   /* using arrays to create checklist*/
   $("#add-update-msg").hide();
+  $("#add-coin-msg").hide();
 
 
 
@@ -74,6 +85,7 @@ $(document).ready(function () {
       checklist.push(Item);
       localStorage.setItem("Checklist", checklist);
     }
+
     document.getElementById("list").innerHTML = checklist
 
 
@@ -93,105 +105,15 @@ $(document).ready(function () {
   $("#addPoints").click(function (e) {
     e.preventDefault();
     addition()
+    $("#add-coin-msg").show().fadeOut(3000);
   })
 
-  /*const APIKEY = "60150aff6adfba69db8b6b87";
-  updateList()
-  $("#add-update-msg").hide();
-
-  
-  $("#addItem").on("click", function (e) {
-    //prevent default action of the button 
-    e.preventDefault();
-
-    let item = $("#addingItem").val()
-
-
-    let jsondata = {
-      "checklist": item,
-    };
-
-
-    let settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://forgetmenot-7aac.restdb.io/rest/checklist",
-      "method": "GET",
-      "headers": {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache"
-      },
-
-
-      "processData": false,
-      "data": JSON.stringify(jsondata),
-      "beforeSend": function(){
-        //@TODO use loading bar instead
-        //disable our button or show loading bar
-        $("#addItem").prop( "disabled", true);
-
-      }
-    }
-
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      
-      $("#addItem").prop( "disabled", false);
-      
-      //@TODO update frontend UI 
-      $("#add-update-msg").show().fadeOut(3000);
-
-      //update our list
-      updateList();
-    });
-
-
-
-
-
-
-
-  })
-
-  function updateList(limit = 10, all = true) {
-
-    //[STEP 7]: Create our AJAX settings
-    let settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://forgetmenot-7aac.restdb.io/rest/checklist",
-      "method": "GET", // use GET to retrieve info
-      "headers": {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache"
-      },
-    }
-    $.ajax(settings).done(function (response) {
-
-      let content = "";
-
-      for (var i = 0; i < response.length && i < limit; i++) {
-        content = `${response[i].checklist}`
-
-      }
-
-      $("#list").html(content);
-
-      $("#total-items").html(response.length);
-    })
-  }
-
-  
-
-*/
-
+ 
 
 })
 
 function addition() {
-  
+
   let points = localStorage.getItem("Points")   
 
   if(isNaN(points)){
