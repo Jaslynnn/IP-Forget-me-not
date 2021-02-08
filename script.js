d1784446ebed1afb309809410d69978c4cd0db58
Jaslynnn <s10204654@connect.np.edu.sg>
2021-02-08 18:35:19 +0800

    Fixing the shop so that things can be bought

--- script.js
+++ script.js
@@ -1,18 +1,170 @@
-// record the user's house door location
-
-let points = localStorage.getItem("Points")  
+//Display the existing data 
+let points = localStorage.getItem("Points")
 document.getElementById("points").innerHTML = points
-
-let checklist = localStorage.getItem("Checklist")
-var checklistArr = checklist.split(',');
-for (var i = 1; i < checklistArr.length; i++) {
-  document.getElementById("list").innerHTML +=
-    ` ${i} ${checklistArr[i] } <br>   `
+//function to display all statistics like water, plants and coins
+function existing(){
+  waterCount= localStorage.getItem("Water")
+  document.getElementById("waterCount").innerHTML = waterCount
+  waterCount= localStorage.getItem("Points")
+  document.getElementById("points").innerHTML = waterCount
+  waterCount= localStorage.getItem("Plants")
+  document.getElementById("plantsCount").innerHTML = waterCount
 }
 
-document.getElementById("myForm").style.display = "none";
 
 
+
+//jquery
+
+$(document).ready(function () {
+  $("#purchase-confirm1").hide();
+  $("#purchase-confirm2").hide();
+  //shop page
+  $("#Seed1").click(function (e) {
+    e.preventDefault();
+    $("#purchase-confirm1").show();
+  })
+  //deduct coins and add a plant to inventory
+  $("#Deduct5").click(function (e) {
+    e.preventDefault();
+    deductCoins1()
+
+  })
+  $("#close1").click(function (e) {
+    e.preventDefault();
+    $("#purchase-confirm1").hide();
+
+  })
+
+
+
+  //Checklist page
+  /* using arrays to create checklist*/
+  $("#myForm").hide();
+  $("#add-update-msg").hide();
+  $("#add-coin-msg").hide();
+
+
+  $("#addItem").click(function (e) {
+    e.preventDefault();
+    let items = localStorage.getItem("Checklist")
+    let checklist = [items];
+    checklist.forEach(retreival);
+
+    function retreival() {
+      let Item = document.getElementById("addingItem").value
+      checklist.push(Item);
+      localStorage.setItem("Checklist", checklist);
+    }
+    displayChecklist()
+    $("#add-update-msg").show().fadeOut(4000);
+  })
+
+
+  /* function to add points*/
+  $("#addPoints").click(function (e) {
+    e.preventDefault();
+    addition()
+    $("#add-coin-msg").show().fadeOut(4000);
+  })
+
+//Settings page
+
+
+
+
+})
+
+
+//End of jquery, all the javascript functions
+
+//shop
+//Buying of first seed
+function deductCoins1() {
+  let coins = localStorage.getItem("Points")
+  let finalPoints = parseInt(coins) - parseInt('5')
+  localStorage.setItem("Points", finalPoints);
+  document.getElementById("points").innerHTML = finalPoints
+}
+
+//Buying of second seed
+function deductCoins2() {
+  let coins = localStorage.getItem("Points")
+  let finalPoints = parseInt(coins) - parseInt('10')
+  localStorage.setItem("Points", finalPoints);
+  document.getElementById("points").innerHTML = finalPoints
+}
+//Buying of Third seed
+function deductCoins3() {
+  let coins = localStorage.getItem("Points")
+  let finalPoints = parseInt(coins) - parseInt('20')
+  localStorage.setItem("Points", finalPoints);
+  document.getElementById("points").innerHTML = finalPoints
+}
+
+//buying of water
+//deduct coins and add water
+function deductCoinsForWater() {
+  let coins = localStorage.getItem("Points")
+  let finalPoints = parseInt(coins) - parseInt('10')
+  localStorage.setItem("Points", finalPoints);
+  document.getElementById("points").innerHTML = finalPoints
+}
+
+
+//Checklist Page
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+
+function displayChecklist() {
+  let checklist = localStorage.getItem("Checklist")
+  var checklistArr = checklist.split(',');
+  let i = checklistArr.length - 1
+  let lastItem = checklistArr[checklistArr.length - 1]
+  document.getElementById("list").innerHTML +=
+    ` ${i} ${lastItem } <br>   `
+
+}
+
+
+
+function addition() {
+
+  let points = localStorage.getItem("Points")
+
+  if (isNaN(points)) {
+    let points = 0
+    localStorage.setItem("Points", points);
+    let oldpoints = localStorage.getItem("Points")
+    let oldPoints = parseInt(oldpoints)
+    let newPoints = oldPoints + parseInt("5")
+    localStorage.setItem("Points", newPoints);
+    document.getElementById("points").innerHTML += newPoints
+
+  } else {
+
+    localStorage.setItem("Points", points);
+    let oldpoints = localStorage.getItem("Points")
+    let oldPoints = parseInt(oldpoints)
+    let newPoints = oldPoints + parseInt("5")
+    localStorage.setItem("Points", newPoints);
+    document.getElementById("points").innerHTML = newPoints
+  }
+
+
+}
+
+
+//Settings page
+// record the user's house door location
 function recordPosition() {
   const successCallback = (position) => {
     console.log(position);
@@ -43,94 +195,3 @@ function currentLocation() {
 
 
 
-//Checklist page
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
-
-
-
-
-
-
-
-
-$(document).ready(function () {
-  /* using arrays to create checklist*/
-  $("#add-update-msg").hide();
-  $("#add-coin-msg").hide();
-
-
-
-  $("#addItem").click(function (e) {
-
-    e.preventDefault();
-    let items = localStorage.getItem("Checklist")
-    let checklist = [items];
-    checklist.forEach(retreival);
-
-    function retreival() {
-      let Item = document.getElementById("addingItem").value
-      checklist.push(Item);
-      localStorage.setItem("Checklist", checklist);
-    }
-
-  displayChecklist()
-
-
-    $("#add-update-msg").show().fadeOut(4000);
-
-  })
-
-
-  /* function to add points*/
-
-  $("#addPoints").click(function (e) {
-    e.preventDefault();
-    addition()
-    $("#add-coin-msg").show().fadeOut(4000);
-  })
-
- 
-
-})
-function displayChecklist(){
-    let checklist = localStorage.getItem("Checklist")
-    var checklistArr = checklist.split(',');
-    let i = checklistArr.length - 1
-    let lastItem = checklistArr[checklistArr.length-1]
-      document.getElementById("list").innerHTML +=
-        ` ${i} ${lastItem } <br>   `
-
-  }
-
-function addition() {
-
-  let points = localStorage.getItem("Points")   
-
-  if(isNaN(points)){
-    let points = 0
-    localStorage.setItem("Points", points);
-    let oldpoints = localStorage.getItem("Points")    
-    let oldPoints = parseInt(oldpoints)
-    let newPoints = oldPoints + parseInt("5")
-    localStorage.setItem("Points", newPoints);
-    document.getElementById("points").innerHTML += newPoints
-
-  }else{
-
-  localStorage.setItem("Points", points);
-  let oldpoints = localStorage.getItem("Points")    
-  let oldPoints = parseInt(oldpoints)
-  let newPoints = oldPoints + parseInt("5")
-  localStorage.setItem("Points", newPoints);
-  document.getElementById("points").innerHTML = newPoints
-}
-
-
-}
